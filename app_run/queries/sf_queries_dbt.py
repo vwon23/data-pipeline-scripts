9c8760cce6d83e9ec3e744e79e28aea421d98791
vwon23 <vwon87@gmail.com>
2024-05-16 23:59:49 -0700

    add script sending query result in E-mail

--- app_run/queries/sf_queries_dbt.py
+++ app_run/queries/sf_queries_dbt.py
@@ -10,7 +10,7 @@ create_table_customers = """
     create table raw.jaffle_shop.customers
     (   id integer,
         first_name varchar,
-        last_name varchar
+        last_name varchar,
         _etl_loaded_at timestamp default current_timestamp
     );
 """
@@ -27,11 +27,11 @@ load_table_customers = """
 
 create_table_orders = """
     create table raw.jaffle_shop.orders
-    ( id integer,
-    user_id integer,
-    order_date date,
-    status varchar,
-    _etl_loaded_at timestamp default current_timestamp
+    (   id integer,
+        user_id integer,
+        order_date date,
+        status varchar,
+        _etl_loaded_at timestamp default current_timestamp
     );
 """
 
@@ -47,13 +47,13 @@ load_table_orders = """
 
 create_table_payment = """
     create table raw.stripe.payment
-    ( id integer,
-    orderid integer,
-    paymentmethod varchar,
-    status varchar,
-    amount integer,
-    created date,
-    _etl_loaded_at timestamp default current_timestamp
+    (   id integer,
+        orderid integer,
+        paymentmethod varchar,
+        status varchar,
+        amount integer,
+        created date,
+        _etl_loaded_at timestamp default current_timestamp
     );
 """
 
@@ -71,4 +71,18 @@ drop_wh_dbs = """
     drop warehouse transforming;
     drop database raw;
     drop database analytics;
+"""
+
+failed_rule = """
+    with payments as (
+        select * from analytics.vwon.stg_payments
+    )
+
+    select
+        order_id,
+        sum(amount) as total_amount
+    from payments
+
+    group by order_id
+    having total_amount > 50
 """
